68d90dfd26f9e08280ccd1af1cd44b837dc0bd1b
jordyortiz2011 <jordyortiz2011@gmail.com>
2017-06-01 14:02:32 -0500

    Add files via upload

---
+++ public/recursos/indicadores/js/registrar_select.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	
+	$('.chosen-select').chosen({allow_single_deselect:true}); 
+		//resize the chosen on window resize
+
+	
+	
+});
+		
+		
---
+++ public/recursos/sistema/js/usuarios_tabla.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	
+		
+    $('#dynamic-table').DataTable({
+    	language: lenguajeDatatable, 
+    	lengthChange : false //deshabilita select , para mostrar cantidad de resultados
+    });
+
+	
+});//fin document
