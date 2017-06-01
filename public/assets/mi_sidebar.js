d252f95df49ebf299cb287562742f8f5e6426428
jordyortiz2011 <jordyortiz2011@gmail.com>
2017-06-01 14:59:37 -0500

    Add files via upload

--- public/assets/mi_sidebar.js
+++ public/assets/mi_sidebar.js
@@ -1,22 +1,7 @@
-/**
- * Resize function without multiple trigger
- * 
- * Usage:
- * $(window).smartresize(function(){  
- *     // code here
- * });
- */
-
-
 
 // Sidebar
 $(document).ready(function() {
-    // TODO: This is some kind of easy fix, maybe we can improve this
-	 let CURRENT_URL = window.location.href.split('?')[0];
-     let SIDEBAR = $('.nav-list');
-
-    SIDEBAR.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('active');
-
-
+   
+	
 
 });
